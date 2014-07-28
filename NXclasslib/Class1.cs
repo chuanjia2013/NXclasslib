45abfe22a521f7d49525e572712fc1304acc7fe3
chuanjia2013 <zhangchuanjia2008@163.com>
2014-07-28 15:34:52 +0800

    添加程序入口函数

--- NXclasslib/Class1.cs
+++ NXclasslib/Class1.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Forms;
 
 namespace NXclasslib
 {
     public class Class1
     {
+        public static int Main(string[] Args)
+        {
+            try
+            {
+                MessageBox.Show("HOLLE,MY WORLD!");
+                return 0;
+            }
+            catch (Exception)
+            {
+                return 1;
+            }
+
+        }
     }
 }
